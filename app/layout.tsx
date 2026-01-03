1c28c6f330e06c6eea519f656257d65dadc421e2
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2026-01-03 17:31:21 +0100

    création des dossiers et fichiers nécéssaires  dans le dossier app

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import { Inter} from "next/font/google";
 import Link from "next/link";
 
-const inter = Inter({ susbsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"]});
 
 export default function RootLayout({children}: {children: React.ReactNode}){
   return(
