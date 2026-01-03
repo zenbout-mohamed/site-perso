30cd8eeaf4d24b25e86696f5d5c2817f4a613b5c
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2026-01-03 17:39:01 +0100

    Header.tsx réalisé

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <Link href="/">Accueil</Link>
+        <Link href="/about">À propos</Link>
+        <Link href="/projects">Projets</Link>
+        <Link href="/contact">Contact</Link>
+      </nav>
+    </header>
+  );
+}
